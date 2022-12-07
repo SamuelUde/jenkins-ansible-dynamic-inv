b114501a557c5ecd02dcebe8ecc365994947f529
SamuelUde <71829810+SamuelUde@users.noreply.github.com>
2022-12-08 00:26:53 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
       region = "eu-north-1c"
 }
 resource "aws_instance" "app" {
-    ami           = "ami-08e637cea2f053dfa"
+    ami           = "ami-02384a901b5df8024" 
     instance_type = "t3.micro"
     security_groups = ["jkSG"]
     key_name      =  "pem-cer-KeyFile"
