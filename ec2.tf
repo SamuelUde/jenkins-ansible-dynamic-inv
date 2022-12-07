faaeeafe419b0b5a88bf904f2f8f4c3814d5540c
SamuelUde <71829810+SamuelUde@users.noreply.github.com>
2022-12-08 00:18:16 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-      region = "us-east-1"
+      region = "eu-north-1c"
 }
 resource "aws_instance" "app" {
     ami           = "ami-08e637cea2f053dfa"
     instance_type = "t3.micro"
     security_groups = ["jkSG"]
-    key_name      =  "key29"
+    key_name      =  "pem-cer-KeyFile"
     tags = {
         Name = " tomcat"
         Environment = "dev"
