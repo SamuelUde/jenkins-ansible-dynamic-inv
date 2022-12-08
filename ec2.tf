d40230cf96511d630087370f0d6a74391cbaf5af
SamuelUde <71829810+SamuelUde@users.noreply.github.com>
2022-12-08 02:02:38 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
       region = "eu-north-1"
 }
-resource "aws_instance" "app" {
+resource "aws_instance" "infra" {
     ami           = "ami-02384a901b5df8024" 
     instance_type = "t3.micro"
     security_groups = ["jkSG"]
